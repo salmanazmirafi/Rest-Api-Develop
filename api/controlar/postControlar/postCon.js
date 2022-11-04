3327730d27140c21651636a535ee83a1af71e0b1
Salman Azmi Rafi <salmanazmirafipersonal@gmail.com>
2022-11-04 15:35:27 +0600

    Create Post
    I'm Create Api Create Post Routs

--- api/controlar/postControlar/postCon.js
+++ api/controlar/postControlar/postCon.js
@@ -1,3 +1,23 @@
+const Post = require("../../modules/postModules");
+// Create Post
+
 exports.createPost = async (req, res, next) => {
-  res.send("I'm form post Controlar");
+  const { title, body, username, category, photo } = req.body;
+  try {
+    const post = await Post.create({
+      title,
+      body,
+      username,
+      category,
+      photo,
+    });
+    res.status(201).json({ post });
+  } catch (error) {
+    res.status(404).json({
+      message: "Somthing Went Wrong",
+      error,
+    });
+  }
 };
+// Update Post
+// Delete Post
