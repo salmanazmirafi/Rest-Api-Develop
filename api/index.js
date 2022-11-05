ca869686f1139b65c413564bf3446f3281972a77
Salman Azmi Rafi <salmanazmirafipersonal@gmail.com>
2022-11-05 11:00:26 +0600

    All Rest Api Done

--- api/index.js
+++ api/index.js
@@ -1,15 +1,37 @@
 const express = require("express");
 const app = express();
 const connectBD = require("./config/connectDB");
+const multer = require("multer");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
 const sinUp = require("./Routes/Auth/signUp");
 const login = require("./Routes/Auth/login");
 const userRout = require("./Routes/users/usersRout");
 const postRoute = require("./Routes/postRouts");
 const catagoryRout = require("./Routes/catagorys");
+
 dotenv.config();
 
 app.use(express.json());
+app.use(morgan("dev"));
+
+// file upload
+
+const uploadStoregFile = multer.diskStorage({
+  destination: (req, file, callBack) => {
+    callBack(null, "./image");
+  },
+  filename: (req, file, callBack) => {
+    callBack(null, file.originalname);
+  },
+});
+const upload = multer({ storage: uploadStoregFile });
+
+// upload file
+app.post("/upload", upload.single("file"), (req, res) => {
+  console.log(req.file);
+  res.send("Image Upload Successful😊");
+});
 
 //Routes
 // user Routes
@@ -20,6 +42,8 @@ app.use("/user", userRout);
 app.use("/posts", postRoute);
 //catagorys routs
 app.use("/catagorys", catagoryRout);
+// upload file
+
 //Routes
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
